edae377e9792638c6ca63b27dd0ed635eb86b877
Finlay Raynor <2767798R@student.gla.ac.uk>
2024-02-06 16:37:17 +0000

    Task 10

--- rango/views.py
+++ rango/views.py
@@ -9,34 +9,29 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
-
+from datetime import datetime
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only --  or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!)
-    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-
     page_list = Page.objects.order_by('-views')[:5]
 
-    # Construct a dictionary to pass to the template engine at its context.
-    # Note the key boldmessage matches to {{ boldmessage}} in the template!
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    visitor_cookie_handler(request)
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make out lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Finlay Raynor'}
+    visitor_cookie_handler(request)
+    context_dict ={}
+    context_dict['visits'] = request.session['visits']
+    context_dict['boldmessage'] = 'This tutorial has been put together by Finlay Raynor'
     return render(request, 'rango/about.html', context=context_dict)
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -51,6 +46,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
 
 @login_required
 def add_category(request):
@@ -75,6 +71,7 @@ def add_category(request):
         # Will handle the bad form, new form, or no form supplied cases.
         # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
+
 
 @login_required
 def add_page(request, category_name_slug):
@@ -109,6 +106,7 @@ def add_page(request, category_name_slug):
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
 
+
 def register(request):
     registered = False
 
@@ -140,6 +138,7 @@ def register(request):
                              'profile_form': profile_form,
                              'registered': registered})
 
+
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -161,11 +160,36 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
     
+
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
+
 
 @login_required
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
